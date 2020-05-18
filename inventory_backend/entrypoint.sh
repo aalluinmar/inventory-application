576169e3b464a8a84bd080d69dc2818f6b2ec911
aalluinmar <aravind.allu@inmar.com>
2020-05-18 22:51:16 +0530

    initial backend structure

---
+++ inventory_backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# wait for PSQL to server to start
+sleep 5
+echo "Django Configuration"
+
+# prepare init migration
+echo "prepare init migration"
+source /code/dev.env
+python /code/manage.py makemigrations
+# migrate db, so we have the latest db schema
+echo "migrate db"
+python /code/manage.py migrate
+
+# start development server on public ip interface, on port 8000
+python /code/manage.py runserver 0.0.0.0:8000
