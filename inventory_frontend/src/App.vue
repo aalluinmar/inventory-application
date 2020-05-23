f1471c00f165bf8a49940d2dfcd0f6e6dbc97903
aalluinmar <aravind.allu@inmar.com>
2020-05-23 23:59:38 +0530

    basic frontend project structure

---
+++ inventory_frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+	<router-view></router-view>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return {
+    	msg: 'App',
+		};
+	},
+	data() {
+		return {
+			base: 'I am a Base Component',
+			validateUser: false
+		};
+	},
+	methods: {
+		logout() {
+			this.$auth.logOut();
+		},
+	}
+};
+</script>
+<style>
+</style>
